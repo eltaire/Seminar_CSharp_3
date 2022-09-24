569849acbc24e83b03c5faca06619db333f979ba
Elta <eltaire@gmail.com>
2022-09-24 19:32:43 +0300

    Homework

---
+++ Task19_HW_math/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void CheckPalindrom(int nmb)
+{
+    if(nmb >= 10000 && nmb < 100000)
+    {
+        int div1 = nmb / 10000;
+        int ost1 = nmb % 10;
+             if(div1 == ost1)
+            {
+                nmb = nmb / 10;
+                int div2 = (nmb/ 100) % 10;
+                int ost2 = nmb % 10;
+                if(div2 == ost2)
+                    Console.WriteLine("Ваше число является палиндромом");
+            }
+            else 
+            Console.WriteLine("Ваше число не является палиндромом");     
+    }
+    else
+    Console.WriteLine("Ваше число не пятизначное");
+}
+CheckPalindrom(number);
--- Task19_HW_math/obj/Debug/net6.0/Task19_HW_math.AssemblyInfo.cs
+++ Task19_HW_math/obj/Debug/net6.0/Task19_HW_math.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Task19_HW")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task19_HW_math")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Task19_HW")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Task19_HW")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task19_HW_math")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task19_HW_math")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
